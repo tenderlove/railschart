1d9881c0df289ad2eabe5204b6f50f9779cb78df
Aaron Patterson <aaron.patterson@gmail.com>
2012-01-17 16:05:39 -0800

    adding from and to

--- Rakefile
+++ Rakefile
@@ -2,25 +2,6 @@ $: << 'lib'
 
 require 'travis'
 
-file 'report.html' do
-  t = Travis.new
-  if File.exists? 'builds.marshal'
-    master_builds = Marshal.load File.read 'builds.marshal'
-  else
-    master_builds = t.latest_builds.find_all { |x|
-      x.branch == 'master' && x.passed?
-    }
-    File.write 'builds.marshal', Marshal.dump(master_builds)
-  end
-
-  chart = Travis::Chart.new master_builds.sort_by { |b|
-    b.number.to_i
-  }
-  File.write 'report.html', chart.to_html
-end
-
-task :default => 'report.html'
-
 task :clean do
   rm 'report.html'
   rm 'builds.marshal'
@@ -35,3 +16,5 @@ file 'report.js' do
     f.write "function travisdata() { return #{json}; }"
   }
 end
+
+task :default => 'report.js'
--- lib/travis.rb
+++ lib/travis.rb
@@ -2,72 +2,9 @@ require 'psych'
 require 'net/http'
 require 'uri'
 require 'date'
-require 'chart'
 require 'json'
 
 class Travis
-  Build = Struct.new(:internal, :travis) do
-    private :internal, :travis
-
-    def id
-      internal['id'].to_i
-    end
-
-    def branch
-      internal['branch']
-    end
-
-    def number
-      internal['number']
-    end
-
-    def details
-      @details ||= travis.details id
-    end
-
-    def passed?
-      details.passed?
-    end
-  end
-
-  class Command < Struct.new(:start, :finish, :status, :env)
-    def start
-      DateTime.parse(super).to_time
-    end
-
-    def finish
-      DateTime.parse(super).to_time
-    end
-
-    def duration
-      finish - start
-    end
-  end
-
-  Details = Struct.new(:internal) do
-    private :internal
-
-    def commands
-      internal['matrix'].map { |x|
-        c = Command.new(*x.values_at(*%w{ started_at finished_at status }))
-        c.env = x['config']['env']
-        c
-      }
-    end
-
-    def passed?
-      commands.all? { |x| x.status == 0 }
-    end
-
-    def to_hash
-      internal
-    end
-
-    def to_json
-      JSON.dump to_hash
-    end
-  end
-
   def latest_builds
     uri = URI('http://travis-ci.org/rails/rails/builds.json')
 
